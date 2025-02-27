1d9b6bd80a8ff29e6405cae403bfc190930b20b1
Mateo, Noel John <125169838+NoLmuna@users.noreply.github.com>
2025-02-28 02:04:17 +0800

    Modified Users
    added trainer, trainee, and admin to the user api

--- fitforge_backend/src/controllers/UserController.js
+++ fitforge_backend/src/controllers/UserController.js
@@ -6,12 +6,9 @@ require('dotenv').config();
 
 exports.userLogin = async (req, res) => {
     try {
-
         const { user_email, user_password } = req.body;
 
-        const existingUser = await User.findOne({
-            where: { user_email: user_email }
-        });
+        const existingUser = await User.findOne({ where: { user_email } });
 
         if (!existingUser) {
             return send.sendResponseMessage(res, 404, null, "User not found");
@@ -19,47 +16,46 @@ exports.userLogin = async (req, res) => {
 
         const verifyPassword = await argon2.verify(existingUser.user_password, user_password);
         if (verifyPassword) {
-            const token = jwt.sign({ 
-                id: existingUser.id
-            },process.env.SECRET_KEY, 
-            { 
-                expiresIn: '1h' 
-            });
+            const token = jwt.sign(
+                { id: existingUser.id, role: existingUser.user_role },
+                process.env.SECRET_KEY,
+                { expiresIn: '1h' }
+            );
 
             return send.sendResponseMessage(res, 200, token, "User logged in successfully!");
-
         } else {
-            return res.status(401).json({
-                message: "Invalid credentials"
-            });
+            return res.status(401).json({ message: "Invalid credentials" });
         }
-        
+
     } catch (error) {
-        send.sendErrorMessage(res, 500, error)
+        send.sendErrorMessage(res, 500, error);
     }
-}
+};
 
 exports.registerUser = async (req, res) => {
     try {
-        const { user_name, user_email, user_password } = req.body;
+        const { user_name, user_email, user_password, user_role } = req.body;
 
         const existingUser = await User.findOne({ where: { user_email } });
         if (existingUser) {
             return send.sendResponseMessage(res, 400, null, "Email already in use");
         }
 
+        if (!['trainee', 'trainer', 'admin'].includes(user_role)) {
+            return send.sendResponseMessage(res, 400, null, "Invalid role provided");
+        }
+
         const hash = await argon2.hash(user_password);
         const newUser = await User.create({
             user_name: user_name,
             user_email: user_email,
-            user_password: hash
+            user_password: hash,
+            user_role: user_role || 'trainee'
         });
 
         return send.sendResponseMessage(res, 201, newUser, "User registered successfully");
 
-    }
-    catch (error) {
+    } catch (error) {
         return send.sendErrorMessage(res, 500, error);
     }
-}
-
+};
